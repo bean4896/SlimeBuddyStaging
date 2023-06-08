8049df3eada316e32071b0bd936ef50d934b7230
Ittachi <dkybone1996@gmail.com>
2023-06-08 13:12:12 +0800

    navbar update2

--- slimebuddy/src/components/NavBar.js
+++ slimebuddy/src/components/NavBar.js
@@ -6,7 +6,6 @@ const NavBar = () => {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex items-center">
-          <img src="" className="h-8 mr-3" alt="SlimeBuddy Logo" />
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <Link to="/">SlimeBuddy</Link>
             <Link to="/Mint">Mint</Link>
